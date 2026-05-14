8cfb366c49eda341bcb3d89ee81de8b53b91419e
Oreana Paola Andrade da silva <oreanadev@gmail.com>
2026-05-14 17:54:38 -0400

    aaa

--- server.js
+++ server.js
@@ -131,7 +131,10 @@ pool.query('SELECT NOW()', (err, res) => {
 // --- CONFIGURAR TRANSPORTE DE EMAIL ---
 // Configura el transporte SMTP para poder enviar correos.
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
